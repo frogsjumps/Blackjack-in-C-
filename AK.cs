56db29b8ab29a9989ecf61716b70668662f9727b
Avi Kumar <149712920+frogsjumps@users.noreply.github.com>
2024-02-11 18:21:17 -0800

    Update AK.cs

--- AK.cs
+++ AK.cs
@@ -5,13 +5,14 @@ int betnumber;
 int balance = 1000;
 int playButton;
 int num01;
-int playerCards = 4;
+int playerCards = 2;
 int hit = 2;
 int hitOrStand;
 int cardChoices = 2;
 int grandtotal = 0;
 int cardcoins = 0;
 string enter;
+int playertotal = 0;
 
 Console.WriteLine("Welcome to BlackJack!, press 1 for rules or press 2 to play!");
 playButton = Convert.ToInt32(Console.ReadLine());
@@ -60,8 +61,30 @@ if (playButton == 1)
             cardcoins++;
              for (int i = 0; i < hit; i++)
              {
+                Random numberGen = new Random();
+                num01 = numberGen.Next(1,11);
+                cardDraw.Add(num01);
                 Console.WriteLine($" ___\n|   |\n| {cardDraw[i]} |\n|___|");
              } 
+
+               for (int i = 0; i < cardcoins; i++)
+            {
+                playertotal += cardDraw[i];
+            }
+
+        if (playertotal < 21)
+        {
+            
+        } else if (playertotal == 21)
+        {
+            Console.WriteLine("Blackjack!");
+            cardChoices = 4;
+        } else 
+        {
+            Console.WriteLine("You busted");
+            cardChoices = 4;
+        }
+            
         }
 
         else if (hitOrStand == 2)
@@ -83,14 +106,12 @@ if (playButton == 1)
             {
                 grandtotal += cardDraw[i];
             }
-            Console.WriteLine("You stand. Your grand total is " + grandtotal);
+            Console.WriteLine("Your grand total is " + grandtotal);
         }
         Console.WriteLine("Press enter to continue");
         enter = Console.ReadLine();
 
         Console.Clear();
-
-        
     }else 
     {
         Console.WriteLine("You cannot bet more than you have.");
@@ -101,9 +122,6 @@ if (playButton == 1)
 }
 
 Console.ReadKey();
-
-
-
 
 
 
